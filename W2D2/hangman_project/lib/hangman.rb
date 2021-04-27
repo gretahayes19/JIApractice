a1020610c7498914548e23a6bd9b2b70d0ebd652
Greta <gretahayes19@gmail.com>
2021-04-27 09:19:05 -0400

    first

---
+++ W2D2/hangman_project/lib/hangman.rb
@@ -0,0 +1,42 @@
+class Hangman
+  DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+
+  def self.random_word
+    DICTIONARY.sample
+  end
+
+  def initialize
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length, "_")
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
+
+  def guess_word
+    @guess_word
+  end
+
+  def attempted_chars
+    @attempted_chars
+  end
+
+  def remaining_incorrect_guesses
+    @remaining_incorrect_guesses
+  end
+
+  def already_attempted?(char)
+        if @attempted_chars.include?(char)
+          true
+        else
+          false
+        end
+  end
+
+  def get_matching_indices(char)
+    idxs = []
+    @secret_word.each_with_index do |idx, ele| 
+      if ele == char
+        
+    end
+  end
+end
