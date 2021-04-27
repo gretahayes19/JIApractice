ade5552634c79565b423cce91d1d65d7326e59b1
Greta <gretahayes19@gmail.com>
2021-04-27 10:10:14 -0400

    hotel pt 1

---
+++ W2D2/hotel_project/lib/hotel.rb
@@ -0,0 +1,6 @@
+require_relative "room"
+
+class Hotel
+
+
+end
---
+++ W2D2/hotel_project/lib/room.rb
@@ -0,0 +1,33 @@
+class Room
+  def initialize(cap)
+    @capacity = cap
+    @occupants = []
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def occupants
+    @occupants
+  end
+
+  def full?
+   @occupants.length == @capacity
+
+  end
+
+  def available_space
+    @capacity - @occupants.length
+  end
+
+  def add_occupant(name)
+    if !self.full?
+        @occupants << name 
+        return true
+    end
+    false
+end
+  
+
+end
