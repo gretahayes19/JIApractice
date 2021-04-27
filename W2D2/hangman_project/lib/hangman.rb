b36e26ff5c19efdd202832cfe9cb218b47c13e84
Greta <gretahayes19@gmail.com>
2021-04-27 09:44:58 -0400

    hangman pt2

--- W2D2/hangman_project/lib/hangman.rb
+++ W2D2/hangman_project/lib/hangman.rb
@@ -44,4 +44,52 @@ class Hangman
   def fill_indices(char, indices)
     indices.each { |i| @guess_word[i] = char }
   end
+
+  def try_guess(char)
+    if already_attempted?(char)
+      print "that has already been attempted"
+      return false
+    else
+      @attempted_chars << char
+      indices = get_matching_indices(char)
+
+      if indices.empty?
+        @remaining_incorrect_guesses -= 1 
+      else
+        fill_indices(char, indices)
+      end
+
+      return true
+    end
+  end
+
+  def ask_user_for_guess
+    print "Enter a char:"
+    try_guess(gets.chomp)
+  end
+
+  def win?
+    if @guess_word.join("") == @secret_word
+      print "WIN"
+      return true
+    end
+    false
+  end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      print "LOSE"
+      return true
+    end
+    false
+  end
+
+  def game_over?
+    if self.win? || self.lose? 
+      print @secret_word
+      return true
+    end
+    false
+  end
+
 end
