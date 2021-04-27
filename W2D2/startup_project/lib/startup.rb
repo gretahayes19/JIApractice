d1bc60f807517146aaa3001777762cad1d0d4741
Greta <gretahayes19@gmail.com>
2021-04-27 11:11:07 -0400

    startup part 2

--- W2D2/startup_project/lib/startup.rb
+++ W2D2/startup_project/lib/startup.rb
@@ -2,4 +2,48 @@ require "employee"
 
 class Startup
 
+    attr_reader :name, :funding, :salaries, :employees
+
+    def initialize(name, funding, salaries)
+        @name = name
+        @funding = funding
+        @salaries = salaries
+        @employees = []
+    end
+
+    def valid_title?(title)
+        return true if @salaries[title]
+        false
+    end
+
+    def >(other_startup)
+        self.funding > other_startup.funding
+    end
+
+    def hire(name, title)
+        if self.valid_title?(title)
+            @employees << Employee.new(name, title) 
+        else
+            raise "invalid title"
+        end
+    end
+
+    def size
+        @employees.length
+    end
+
+    def pay_employee(employee)
+        salary = @salaries[employee.title]
+        if salary <= @funding
+            employee.pay(salary)
+            @funding -= salary
+        else
+            raise "not enough funds"
+        end
+    end
+
+    def payday
+        @employees.each {|employee| pay_employee(employee)}
+    end
+
 end
