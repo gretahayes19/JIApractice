26d1ed2bffca99832db915d338cdca88b25180b1
Greta <gretahayes19@gmail.com>
2021-05-05 14:25:27 -0400

    range

--- W3D3/recursion.rb
+++ W3D3/recursion.rb
@@ -1,1 +1,8 @@
-##hi
+##muteable datatypes within shallow dupes will change
+##immutable databtypes will not change
+
+def range(min, max) 
+    return [] if max <= min 
+
+    return range(min, max-1) << max-1
+end
