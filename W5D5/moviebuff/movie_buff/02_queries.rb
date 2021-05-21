48537661d2bea00f350d24e7c339695ae955288d
Greta <gretahayes19@gmail.com>
2021-05-21 11:36:29 -0400

    movie buff part 2

--- W5D5/moviebuff/movie_buff/02_queries.rb
+++ W5D5/moviebuff/movie_buff/02_queries.rb
@@ -35,5 +35,12 @@ end
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
-  
+  Actor
+    .select(:id, :name, 'COUNT(*) as roles')
+    .joins(:castings)
+    .where('castings.ord > 1')
+    .group('actors.id')
+    .order('roles DESC')
+    .limit(2)
+
 end
