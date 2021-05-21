64fa4a1537998b515e1dc3ecfcc49d75acb47677
Greta <gretahayes19@gmail.com>
2021-05-21 12:17:38 -0400

    validations

--- W5D5/polls_app/app/models/answer_choice.rb
+++ W5D5/polls_app/app/models/answer_choice.rb
@@ -9,6 +9,8 @@
 #  updated_at  :datetime         not null
 #
 class AnswerChoice < ApplicationRecord
+    validates :text, presence: true 
+    
     belongs_to :question,
         primary_key: :id,
         foreign_key: :question_id, 
